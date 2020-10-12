7471e038a6789396ff23819b04e5630b9e12527c
Saulo Castilhos <saulo.castilhos@elevenfinancial.com>
2020-10-12 16:20:56 -0300

    add: structure

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,11 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FiPlus } from "react-icons/fi";
 
-import "../styles/pages/landing.css";
+import mapMarkerImg from "../images/map-marker.svg";
+
+import "../styles/pages/orphanages-map.css";
 
 function OrphanagesMap() {
   return (
-    <div>
-      <h1>OrphanagesMap</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="happy" />
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Cubatão</strong>
+          <span>São Paulo</span>
+        </footer>
+      </aside>
+
+      <div></div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#ffffff" />
+      </Link>
     </div>
   );
 }
