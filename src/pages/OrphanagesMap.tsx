76f44ef22d81d6df643fe4b339eb67218a7b6192
Saulo Castilhos <saulo.castilhos@elevenfinancial.com>
2020-10-12 16:00:35 -0300

    fix: separating landing page && add: orphanages

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/pages/landing.css";
+
+function OrphanagesMap() {
+  return (
+    <div>
+      <h1>OrphanagesMap</h1>
+    </div>
+  );
+}
+
+export default OrphanagesMap;
